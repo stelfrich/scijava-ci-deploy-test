01a45ed3085e582ac0aa6072e686c564343ecd29
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2017-11-16 07:41:21 +0100

    AppVeyor: beautify outputs

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -10,7 +10,7 @@ $Env:Path
 "`n== Java =="
 & "cmd.exe" /c 'java -version 2>&1' # Output goes to STDERR which makes this invocation fail the build in PS
 
-"== Building the artifact locally only =="
+"`n== Building the artifact locally only =="
 & "mvn" "-B" "install" 2> $null
 
 exit $LASTEXITCODE
