e3a593a3911269bcfd954ea67b536d5811d3130e
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2017-11-16 07:36:50 +0100

    AppVeyor: minimize footprint for general projects

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -1,3 +1,15 @@
+"== Path (Execution Environment) =="
+$Env:Path
+
+"`n== Path (Machine) =="
+[Environment]::GetEnvironmentVariable("Path", "Machine")
+
+"`n== Maven =="
+& "mvn" "-B" "--version"
+
+"`n== Java =="
+& "cmd.exe" /c 'java -version 2>&1' # Output goes to STDERR which makes this invocation fail the build in PS
+
 "== Building the artifact locally only =="
 & "mvn" "-B" "install" 2> $null
 
